ab6ab85fe4940f0dbae8d5a1c80c2cc57029d3f2
Roque Santos <roque-100@hotmail.com>
2024-10-25 22:57:58 -0300

    added a simple protocol for receive/send data streams

--- RedisClient/RedisClient.cpp
+++ RedisClient/RedisClient.cpp
@@ -1,8 +1,107 @@
+#include <cassert>
 #include <iostream>
 #include <winsock2.h>
 #include <ws2tcpip.h>
 
 #pragma comment(lib, "Ws2_32.lib")
+
+const size_t k_max_msg = 4096;
+
+// TODO: Move this to an external library, this is copied from RedisServer.cpp
+static int32_t read_full(int conn, char* buf, size_t n)
+{
+    while (n > 0)
+    {
+        SSIZE_T rv = recv(conn, buf, n, 0);
+
+        if (rv <= 0)
+        {
+            return -1;
+        }
+        assert(static_cast<size_t>(rv) <= n);
+
+        n -= static_cast<size_t>(rv);
+        buf += rv;
+    }
+
+    return 0;
+}
+
+// TODO: Move this to an external library, this is copied from RedisServer.cpp
+static int32_t write_all(int conn, char* buf, size_t n)
+{
+    while (n > 0)
+    {
+        SSIZE_T rv = send(conn, buf, n, 0);
+
+        if (rv <= 0)
+        {
+            return -1;
+        }
+
+        assert(static_cast<size_t>(rv) <= n);
+        n -= static_cast<size_t>(rv);
+        buf += rv;
+    }
+    
+    return 0;
+}
+
+static int32_t query(int conn, const char *text)
+{
+    uint32_t len = static_cast<uint32_t>(strlen(text));
+    if (len > k_max_msg)
+    {
+        return -1;
+    }
+
+    char wbuf[4 + k_max_msg];
+    memcpy(wbuf, &len, 4);
+    memcpy(&wbuf[4], text, len);
+    if (int32_t err = write_all(conn, wbuf, 4 + len))
+    {
+        return err;
+    }
+
+    // 4 bytes header
+    char rbuf[4 + k_max_msg + 1];
+    errno = 0;
+
+    int32_t err = read_full(conn, rbuf, 4);
+    if (err)
+    {
+        if (errno == 0)
+        {
+            std::cout << "EOF\n";
+        }
+        else
+        {
+            std::cout << "Error handling query: read() error\n";
+        }
+
+        return err;
+    }
+
+    memcpy(&len, rbuf, 4);
+    if (len > k_max_msg)
+    {
+        std::cout << "Message length is too large\n";
+        return -1;
+    }
+
+    // reply body
+    err = read_full(conn, &rbuf[4], len);
+    if (err)
+    {
+        std::cout << "Error reading data";
+        return err;
+    }
+
+    // do something
+    rbuf[4 + len] = '\0';
+    std::cout << &rbuf[4] << "\n";
+    return 0;
+}
 
 int main() {
     WORD versionWanted = MAKEWORD(1, 1);
@@ -23,22 +122,46 @@ int main() {
     
     int statusCode = connect(socketProc, (const struct sockaddr*)&addr, sizeof(addr));
     if (statusCode) {
-        std::cout << "Failed to connect to " << addr.sin_addr.s_addr << ":" << 1234 << " (code: " << statusCode << ")" << std::endl;
+        std::cout << "Failed to connect to " << addr.sin_addr.s_addr << ":" << 1234 << " (code: " << statusCode << ")\n";
         WSACleanup();
         return 1;
     }
 
-    char msg[] = "hello";
-    send(socketProc, msg, strlen(msg), 0);
+    // Page 3 testing:
+    // char msg[] = "hello";
+    // send(socketProc, msg, strlen(msg), 0);
+    //
+    // char rbuf[64] = {};
+    // SSIZE_T n = recv(socketProc, rbuf, sizeof(rbuf) - 1, 0);
+    // if (n < 0) {
+    //     std::cout << "Failed to read buffer" << std::endl;
+    //     WSACleanup();
+    //     return 1;
+    // }
+    // std::cout << "server says: " << rbuf << std::endl;
 
-    char rbuf[64] = {};
-    SSIZE_T n = recv(socketProc, rbuf, sizeof(rbuf) - 1, 0);
-    if (n < 0) {
-        std::cout << "Failed to read buffer" << std::endl;
-        WSACleanup();
-        return 1;
+    // Multiple requests
+    int32_t err = query(socketProc, "Hello World!");
+    if (err)
+    {
+        // i'm fr using goto
+        goto L_DONE;
     }
 
-    std::cout << "server says: " << rbuf << std::endl;
+    err = query(socketProc, "Hello once again!");
+    if (err)
+    {
+        // i'm fr using goto
+        goto L_DONE;
+    }
+
+    err = query(socketProc, "Hello one more time!");
+    if (err)
+    {
+        // i'm fr using goto
+        goto L_DONE;
+    }
+
+L_DONE:
     closesocket(socketProc);
 }
--- RedisServer/RedisServer.cpp
+++ RedisServer/RedisServer.cpp
@@ -5,6 +5,7 @@ https://build-your-own.org/redis/03_hello_cs
 Roque Santos - 2024-10-24
 */
 
+#include <assert.h>
 #include <iostream>
 #include <winsock2.h>
 #include <ws2tcpip.h>
@@ -14,6 +15,8 @@ Roque Santos - 2024-10-24
 // This #pragma doesn't work with g++ compiler (was using this before changing from VSCode to Rider
 // to circumvent this, the option "-lwsock32" was added on compiler options
 #pragma comment(lib, "Ws2_32.lib")
+
+const size_t k_max_msg = 4096;
 
 static void do_something(int conn) {
 
@@ -29,16 +32,108 @@ static void do_something(int conn) {
         return;
     }
 
-    std::cout << "client says: " << rbuf << std::endl;
+    std::cout << "client says: " << rbuf << "\n";
 
     // write buffer - "response"
     char wbuf[] = "world";
     send(conn, wbuf, strlen(wbuf), 0);
 }
 
+static int32_t read_full(int conn, char* buf, size_t n)
+{
+    while (n > 0)
+    {
+        SSIZE_T rv = recv(conn, buf, n, 0);
+
+        if (rv <= 0)
+        {
+            return -1;
+        }
+        assert(static_cast<size_t>(rv) <= n);
+
+        n -= static_cast<size_t>(rv);
+        buf += rv;
+    }
+
+    return 0;
+}
+
+static int32_t write_all(int conn, char* buf, size_t n)
+{
+    while (n > 0)
+    {
+        SSIZE_T rv = send(conn, buf, n, 0);
+
+        if (rv <= 0)
+        {
+            return -1;
+        }
+
+        assert(static_cast<size_t>(rv) <= n);
+        n -= static_cast<size_t>(rv);
+        buf += rv;
+    }
+    
+    return 0;
+}
+
 static int32_t one_request(int conn)
 {
-    return 0;
+    // 4 bytes header
+    // (32bit int with buf size) + (max buf len) + (1bit buf end)
+    char rbuf[4 + k_max_msg + 1];
+    errno = 0;
+
+    int32_t err = read_full(conn, rbuf, 4);
+    if (err)
+    {
+        if (errno == 0)
+        {
+            std::cout << "EOF\n";
+        }
+        else
+        {
+            std::cout << "Error handling request " << conn << ": read() error\n";
+        }
+
+        return err;
+    }
+
+    uint32_t len = 0;
+    memcpy(&len, rbuf, 4);
+
+    if (len > k_max_msg)
+    {
+        std::cout << "Message too long\n";
+        return -1;
+    }
+
+    // request body
+    err = read_full(conn, &rbuf[4], len);
+    if (err)
+    {
+        std::cout << "read() error\n";
+        return err;
+    }
+
+    // do something
+    rbuf[4 + len] = '\0';
+    std::cout << "client says: " << &rbuf[4] << "\n";
+
+    // reply using same protocol
+    const char reply[] = "world";
+    char wbuf[4 + sizeof(reply)];
+    len = static_cast<uint32_t>(strlen(reply));
+
+    // 1. copies the reply length to the first 4 bytes of the write buffer
+    //    the first 4 bytes of the stream is the content length (as a 32 bits unsigned integer) to the peer
+    memcpy(wbuf, &len, 4);
+
+    // 2. copies the reply content to the buffer starting at the 4th position
+    //    (offsetting the first 4 bytes that are reserved for the content length)
+    memcpy(&wbuf[4], reply, len); 
+
+    return write_all(conn, wbuf, 4 + len);
 }
 
 int main() {
